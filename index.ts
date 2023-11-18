3c7e2ef7c5137de7626dcc0e3e8fbe82d87cfd8a
Qasim_Nawaz786 <qnawaz857@gmail.com>
2023-11-18 15:56:46 +0500

    Calculator make it easy in use

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import { add } from "./add.js";
+import inquirer from "inquirer";
+
+const asnwers = await inquirer.prompt([
+  {
+    name: "num1",
+    message: "Enter first number",
+    type: "number",
+  },
+  {
+    name: "num2",
+    message: "Enter second number",
+    type: "number",
+  },
+]);
+
+const result = add(asnwers.num1, asnwers.num2);
+console.log(
+  `The result of sum ${asnwers.num1} and ${asnwers.num2} is `,
+  result
+);
